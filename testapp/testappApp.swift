46b667ac651c4cc5094f0ccff93d1014cc06b4f3
MubarakAlseif <Mobarak.Alseif@tuwaiq.edu.sa>
2023-10-05 16:55:53 +0300

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Mubarak on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
